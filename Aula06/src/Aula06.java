85ccd9efd12866a42147719594283bf8fa1fb4be
Letícia Santos <110691992+leticiadssantos@users.noreply.github.com>
2022-10-16 19:30:44 -0300

    Add files via upload

---
+++ Aula06/src/Aula06.java
@@ -0,0 +1,16 @@
+
+public class Aula06 {
+
+	public static void main(String[] args) {
+		ControleRemoto c = new ControleRemoto();
+		c.ligar();
+		c.maisVolume();
+		c.abrirMenu();
+		c.ligarMudo();
+		c.maisVolume();
+		c.fecharMenu();
+	
+
+	}
+
+}
