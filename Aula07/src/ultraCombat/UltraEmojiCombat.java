d6e66620f47821eb664082e7dafee0a6aa5b99e9
Letícia Santos <110691992+leticiadssantos@users.noreply.github.com>
2022-10-21 00:46:54 -0300

    Add files via upload

---
+++ Aula07/src/ultraCombat/UltraEmojiCombat.java
@@ -0,0 +1,12 @@
+package ultraCombat;
+
+public class UltraEmojiCombat {
+
+	public static void main(String[] args) {
+	Lutador l = new Lutador ("Pretty boy", "França", 31, 1.75f, 68.9f, 11, 2, 1 );
+	
+	l.status();
+
+	}
+
+}
