21d2cbb157a62b08f596094cf2a76328a9ab9351
leticiadssantos <le.123dasilva@gmail.com>
2022-10-23 01:48:59 -0300

    teste de commit

---
+++ AtividadeEmJava/src/edu/cursoemvideo/aula06/Main.java
@@ -0,0 +1,17 @@
+package edu.cursoemvideo.aula06;
+
+public class Main {
+	public static void main(String[] args) {
+		ControleRemoto c = new ControleRemoto();
+		c.ligar();
+		c.maisVolume();
+		c.abrirMenu();
+		c.ligarMudo();
+		c.maisVolume();
+		c.fecharMenu();
+	
+
+	}
+
+
+}
